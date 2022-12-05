fce37322f38896c52575c10e88980888a4853a30
Ivan Ustyantsev <vangold915@gmail.com>
2022-12-05 14:19:23 +0700

    Corrected remarks

--- task1_base.py
+++ task1_base.py
@@ -14,9 +14,25 @@ def get_division(numerator, denominator):
     if denominator == 0:
         return "Знаменатель не может быть равен нулю"
     return numerator / denominator
+def get_division_v2(numerator, denominator):
+    """
+     Функция деления двух чисел
+    :param numerator: Числитель
+    :param denominator: Знаменатель
+    :return: результат выполнения деления или, в случае равенства знаменателя
+    нулю, выбрасываем ошибку
+    """
+    try:
+        result = numerator / denominator
+    except ValueError:
+        return 'Value Error'
+    except ZeroDivisionError:
+        return 'Вы не можете делить на ноль!'
+    return result
 
 
 number_1 = int(input("Введите первое число (числитель): "))
 number_2 = int(input("Введите второе число (знаменатель): "))
 
 print(get_division(number_1, number_2))
+print(get_division_v2(number_1, number_2))
